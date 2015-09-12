779dc84fe091d573d39824d0112d6c64612554c4
Kevin Glidewell <kpglide@gmail.com>
2015-09-12 15:49:05 -0400

    updated

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
 
+	$('#todo-input').val('Type your to-do here & press enter');
+	$('#instructions').hide();
+	
 	$('#todo-input').keyup(function(event) {
     	if(event.which == 13) {
         	new_text = $('#todo-input').val();
         	if (!new_text == ''){
-	        	new_elem = "<li>" + new_text + "</li>"
+	        	new_elem = "<li>" + new_text + "</li>";
     	    	$('#todo').prepend(new_elem);
-        		$('#todo-input').val('');
+        		$('#todo-input').val('Type your to-do here & press enter');
+        		$('#instructions').show();
         	}
     	}
     });
