e39d195eed33172a4f38796d5318b659397a29d9
Kevin Glidewell <kpglide@gmail.com>
2015-09-12 21:54:43 -0400

    updated

--- scripts.js
+++ scripts.js
@@ -1,18 +1,54 @@
 $(document).ready(function() {
 
 	$('#todo-input').val('Type your to-do here & press enter');
+	$('#todo-input').click(function() {
+    		$(this).val('');
+    	});
+
+	var placeholder = $('#todo-input').val();
+	$('#todo-input').keydown(function () {
+		if (this.value == placeholder) {
+			this.value = '';
+		}
+	});
+
 	$('#instructions').hide();
 	
 	$('#todo-input').keyup(function(event) {
-    	if(event.which == 13) {
+    	$('.instructions').show();
+
+    	if (event.which == 13) {
         	new_text = $('#todo-input').val();
+        	
         	if (!new_text == ''){
-	        	new_elem = "<li>" + new_text + "</li>";
+	        	new_elem = "<li>" + new_text + "<div class='x'>X</div>" + "</li>";
     	    	$('#todo').prepend(new_elem);
         		$('#todo-input').val('Type your to-do here & press enter');
         		$('#instructions').show();
         	}
+        	$('li').on('dblclick', swipeleftHandler);
+        	$('li').on('mouseenter', showDelete);
+        	$('li').on('mouseleave', hideDelete);
+
+        	$('.x').click(function() {
+        		$(this).parent().remove();
+        	});
     	}
     });
 
+	function swipeleftHandler(event) {
+   		//alert('this works');
+    	if (!$(event.target).hasClass('swipe-left')) {
+	    	$(event.target).addClass('swipe-left');
+	    } else { $(event.target).removeClass('swipe-left'); }
+    }
+
+    function showDelete(event) {
+    	$(event.target).children('.x').show();
+    	$(event.target).children('.x').css('cursor', 'pointer');
+    }
+
+    function hideDelete(event) {
+    	$(event.target).children('.x').hide();
+    }
 });
